93f6a364057630faf17335acb9ed78c6f1bb0093
hlin <Zxsaqw12@&$>
2020-03-10 09:20:07 -0400

    Work in progress

---
+++ src/test/java/io/zipcoder/interfaces/PeopleTest.java
@@ -0,0 +1,13 @@
+package io.zipcoder.interfaces;
+
+import org.junit.Test;
+
+
+public class PeopleTest {
+
+    @Test
+    public void addTest() {
+
+    }
+
+}
--- src/test/java/io/zipcoder/interfaces/PersonTest.java
+++ src/test/java/io/zipcoder/interfaces/PersonTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 
-public class TestPerson {
+public class PersonTest {
 
     @Test
     public void constructorTest() {
